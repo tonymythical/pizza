f52775772bb71c702de6ad6b53be276174686bd8
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-01-27 13:55:37 -0800

    added java stuff

--- pizza.js
+++ pizza.js
@@ -1,1 +1,20 @@
-alert("yum-yum");
+document.getElementById("pizza-form").onsubmit = () => {
+  
+  let isValid = true;
+
+  //Validate first name
+  let fname = document.getElementById("fname").value.trim();
+  if(!fname) {
+    document.getElementById("err-fname").style.display = "block";
+    isValid = false;
+  }
+
+  //Validate last name
+  let lname = document.getElementById("lname").value.trim();
+  if(!lname) {
+    document.getElementById("err-lname").style.display = "block";
+    isValid = false;
+  }
+
+  return isValid;
+};
