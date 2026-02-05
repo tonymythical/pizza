16ba623275061746dde6726d555509f0440e4252
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-02-05 13:32:26 -0800

    e

---
+++ app.js
@@ -0,0 +1,23 @@
+// Import the express module
+import express from 'express';
+
+// Create an instance of an Express application
+const app = express();
+
+// Define the port number where our server will listen
+const PORT = 3000;
+
+// Enable static file serving
+app.use(express.static('public'));
+
+// Define a default "route" ('/')
+// req: contatins inforamtion about the incoming request
+// res: allows us to send back a response to the client
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
