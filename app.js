fd8e8de789d40d2479b1ad99f27786d5549cc8d3
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-03-12 13:31:34 -0700

    fix

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-// Import the express module
 import express from 'express';
 import mysql2 from 'mysql2';
+import dotenv from 'dotenv';
 
-// Create an instance of an Express application
+// Load environment variables from .env
+dotenv.config();
+// console.log(process.env.DB_HOST);
+
 const app = express();
-
-// Define the port number where our server will listen
 const PORT = 3000;
-
-// Enable static file serving
 app.use(express.static('public'));
 
-//Set EJS as the view engine
+// Set EJS as the view engine
 app.set('view engine', 'ejs');
 
 // "Middleware" that allows express to read
 // form data and store it in req.body
 app.use(express.urlencoded({ extended: true }));
 
-// Create a temp array to store orders
-const orders = [];
-
 // Create a pool (bucket) of database connections
-
+const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}).promise();
 
 // Database test route
-app.get('/db-test', async (req, res) => {
+app.get('/db-test', async(req, res) => {
 
     try {
         const pizza_orders = await pool.query('SELECT * FROM orders');
@@ -35,15 +37,13 @@ app.get('/db-test', async (req, res) => {
     }
 });
 
-// Define a default "route" ('/')
-// req: contatins inforamtion about the incoming request
-// res: allows us to send back a response to the client
+// Default route
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 // Contact route
-app.get('/contact', (req, res) => {
+app.get('/contact-us', (req, res) => {
     res.render('contact');
 });
 
@@ -53,30 +53,48 @@ app.get('/thank-you', (req, res) => {
 });
 
 // Admin route
-app.get('/admin', (req, res) => {
-    res.render('admin', { orders });
+app.get('/admin', async(req, res) => {
+
+    // Read all orders from the database
+    // newest first
+    let sql = "SELECT * FROM orders ORDER BY timestamp DESC";
+    const orders = await pool.query(sql);
+    console.log(orders);
+
+    res.render('admin', { orders: orders[0] });
 });
 
-app.post('/submit-order', (req, res) => {
+// Submit order route
+// {"fname":"a","lname":"aa","email":"a",
+// "method":"delivery","toppings":["artichokes"],
+// "size":"small","comment":"","discount":"on"}
+app.post('/submit-order', async(req, res) => {
+    
+    const order = req.body;
 
-    const order = {
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
-        method: req.body.method,
-        toppings: req.body.toppings ? req.body.toppings : "none",
-        size: req.body.size,
-        comments: req.body.comments,
-        timestamp: new Date()
-    };
+    // Create an array of order data
+    // (fname, lname, email, size, method, toppings)
+    const params = [
+        order.fname,
+        order.lname,
+        order.email,
+        order.size,
+        order.method,
+        Array.isArray(order.toppings) ? order.toppings.join(", ") : "none"
+    ];
 
-    // Add order object to orders array 
-    orders.push(order);
+    // Insert a new order into the database
+    const sql = `INSERT INTO orders (fname, lname, email, 
+                 size, method, toppings)
+                 VALUES (?, ?, ?, ?, ?, ?)`;
+
+    const result = await pool.execute(sql, params);
+    console.log("Order inserted with ID: ", result[0].insertId);
 
     res.render('confirmation', { order });
 });
 
-// Start the server and listen on the specified port
+// Listen on the designated port
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
